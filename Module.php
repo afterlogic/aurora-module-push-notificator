f697b1a7350998f0b8ae241e198ec092ce442f01
sash04ek <a.ovcharov@gmail.com>
2020-05-13 11:55:14 +0300

    added write push logs

--- Module.php
+++ Module.php
@@ -6,6 +6,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	public function init()
 	{
 		$this->subscribeEvent('Mail::BeforeDeleteAccount', array($this, 'onBeforeDeleteMailAccount'));
+		\Aurora\System\Router::getInstance()->register($this->GetName(), 'push', [$this, 'onPushRoute']);
 	}
 
 	protected function checkSecret($sSecret)
@@ -17,64 +18,69 @@ class Module extends \Aurora\System\Module\AbstractModule
 		}
 	}
 
-	public function SetPushToken($Email, $Uid, $Token)
+	/**
+	 *
+	 */
+	public function SetPushToken($Uid, $Token, $Emails)
 	{
 		$mResult = false;
 		\Aurora\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
 		$oUser = \Aurora\Api::getAuthenticatedUser();
 		if ($oUser)
 		{
-			$oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccountByEmail($Email, $oUser->EntityId);
-			if ($oAccount && $oAccount->IdUser === $oUser->EntityId)
-			{
-				$oPushToken = (new \Aurora\System\EAV\Query(Classes\PushToken::class))
+				$aPushTokens = (new \Aurora\System\EAV\Query(Classes\PushToken::class))
 					->select()
 					->where(
 						[
-							'$AND' => [
-								'IdAccount' => $oAccount->EntityId,
-								'Uid' => $Uid
-							]
+							'Token' => $Token,
+							'Uid' => $Uid
 						]
 					)
-					->one()
 					->exec();
-				if ($oPushToken)
+				foreach ($aPushTokens as $oPushToken)
 				{
-					$oPushToken->Token = $Token;
-					$mResult = $oPushToken->saveAttribute('Token');
+					if ($oPushToken instanceof Classes\PushToken)
+					{
+						$oPushToken->delete();
+					}
 				}
-				else
+				foreach ($Emails as $sEmail)
 				{
-					$oPushToken = new Classes\PushToken();
-					$oPushToken->IdAccount = $oAccount->EntityId;
-					$oPushToken->Email = $oAccount->Email;
-					$oPushToken->Uid = $Uid;
-					$oPushToken->Token = $Token;
+					$oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccountByEmail($sEmail, $oUser->EntityId);
+					if ($oAccount && $oAccount->IdUser === $oUser->EntityId)
+					{
+						$oPushToken = new Classes\PushToken();
+						$oPushToken->IdAccount = $oAccount->EntityId;
+						$oPushToken->Email = $oAccount->Email;
+						$oPushToken->Uid = $Uid;
+						$oPushToken->Token = $Token;
 
-					$mResult = $oPushToken->save();
+						$mResult = $oPushToken->save();
+					}
 				}
-			}
 		}
 
 		return $mResult;
 	}
 
-	public function SendPush($Secret, $Email, $Message)
+	/**
+	 *
+	 */
+	public function SendPush($Secret, $Email, $Data)
 	{
 		$mResult = [];
 		$this->checkSecret($Secret);
 
 		$aPushTokens = (new \Aurora\System\EAV\Query(Classes\PushToken::class))
-		->select()
-		->where(
-			[
-				'$AND' => [
-					'Email' => $Email,
+			->select()
+			->where(
+				[
+					'$AND' => [
+						'Email' => $Email,
+					]
 				]
-			]
-		)
-		->exec();
+			)
+			->exec();
 
 		$sUrl = 'https://fcm.googleapis.com/fcm/send';
 		$sServerKey = $this->getConfig('ServerKey');
@@ -88,22 +94,27 @@ class Module extends \Aurora\System\Module\AbstractModule
 		{
 			$aRequestBody = [
 				'to' => $oPushToken->Token,
-				'data' => [
-					'message' => $Message
-				],
+				'data' => $Data,
 			];
 			$aFields = json_encode($aRequestBody);
 
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $sUrl);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $aRequestHeaders);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $aFields);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			$mResult[] = curl_exec($ch);
+			curl_setopt_array($ch,
+				[
+					CURLOPT_URL => $sUrl,
+					CURLOPT_CUSTOMREQUEST => 'POST',
+					CURLOPT_HTTPHEADER => $aRequestHeaders,
+					CURLOPT_POSTFIELDS => $aFields,
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_FOLLOWLOCATION => true
+				]
+			);
+
+			$mResult[$oPushToken->Token] = \json_decode(curl_exec($ch), true);
 			curl_close($ch);
 		}
+
+		\Aurora\System\Api::LogObject($mResult, \Aurora\System\Enums\LogLevel::Full, 'push-');
 
 		return $mResult;
 	}
@@ -131,6 +142,48 @@ class Module extends \Aurora\System\Module\AbstractModule
 			foreach ($aPushTokens as $oPushToken)
 			{
 				$oPushToken->delete();
+			}
+		}
+	}
+
+	public function onPushRoute()
+	{
+		$sSecret = isset($_GET['secret']) ? $_GET['secret'] : '';
+		$sEmail = isset($_GET['email']) ? $_GET['email'] : '';
+		$aData = isset($_GET['data']) ? \json_decode($_GET['data'], true) : '';
+		$sPath = isset($_GET['path']) ? $_GET['path'] : '';
+
+		if (!empty($sSecret))
+		{
+			if (!empty($sEmail) && !empty($aData))
+			{
+				echo \json_encode($this->Decorator()->SendPush($sSecret, $sEmail, $aData));
+			}
+			else if (!empty($sPath))
+			{
+				$rEml = \fopen($sPath, 'r');
+				$oMessage = \ZBateson\MailMimeParser\Message::parse($rEml);
+				if ($oMessage)
+				{
+					$oTo = $oMessage->getHeader('To');
+					$sEmail = $oTo->getEmail();
+
+					$oFrom = $oMessage->getHeader('From');
+					$sFrom = $oFrom->getEmail();
+
+					$sSubject = $oMessage->getHeaderValue('Subject');
+					$aData = [
+						"From" => $sFrom,
+						"To" => $sEmail,
+						"Subject" => $sSubject
+					];
+
+					echo \json_encode($this->Decorator()->SendPush($sSecret, $sEmail, $aData));
+				}
+			}
+			else
+			{
+				echo 'Invalid arguments';
 			}
 		}
 	}
